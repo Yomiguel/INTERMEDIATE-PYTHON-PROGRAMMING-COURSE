b8b0ccbc1e757b4c51f43eecd50f973d9c481a2d
Luis Miguel Jimenez <yomiguel112@gmail.com>
2021-09-13 20:31:09 -0500

    created variables.py

---
+++ variables.py
@@ -0,0 +1,67 @@
+#----------------------------STRING VARIBLES---------------------------#
+
+#this is a string type variable in python.
+name = 'Luis'  
+
+#print the sentence.
+print('my name is ' + name)
+
+#this is a number type variable in python.
+age = 30 
+
+#print the sentence.
+print('my name is '+ name + ' and i am '+ str(age) + ' years old')
+
+#this line prints the indicated position of the word.
+print(name[2])
+
+#this line prints the word in capital letter.
+print(name.upper())
+
+#this line print the word in lowercase letter.
+print(name.lower())
+
+#this line print the lengt word.
+print(len(name))
+
+#this line replaces and prints the specified word with another word.
+print(name.replace('Luis', 'hi, my name is '+ name))
+
+#---------------------------------NUMBER VARIBLES---------------------------------------#
+
+#this is a number type variable in python.
+
+number1 = 78
+
+number2 = 101.22
+
+number3 = 4
+
+number4 = -505
+
+#this line prints four numbers.
+
+print(number1, number2, number3, number4)
+
+#this line prints the sum of two numbers.
+print(number1 + number2)
+
+#this line prints the subtraction of two numbers.
+print(number1 - number2)
+
+#this line prints the product of two numbers.
+print(number1 * number2)
+
+#this line prints the remainder of division of two numbers.
+print(number1 % number2)
+
+#this line prints the absolute value of a number.
+print(abs(number1-number2))
+
+#this line prints the maximum value of a list of numbers.
+print(max(number1, number2, number3, number4))
+
+#this line prints the minimun value of a list of numbers.
+print(min(number1, number2, number3, number4))
+
+
