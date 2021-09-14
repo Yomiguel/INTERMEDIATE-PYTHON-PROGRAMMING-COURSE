cfad48efa87a03cd240e72d228dce47a5be77842
Luis Miguel Jimenez <yomiguel112@gmail.com>
2021-09-13 20:50:38 -0500

    created collection.py

---
+++ collections.py
@@ -0,0 +1,6 @@
+#***************************************************collections***************************************
+
+#------------------------------------ counter -------------------------------------------
+from collections import Counter
+
+x = 'Hello this is a sentence, lets see how many a it as'
