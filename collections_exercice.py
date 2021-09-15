69c08111d23a8673a5ff5b58b350a7c96ca8ac18
Luis Miguel Jimenez <yomiguel112@gmail.com>
2021-09-15 18:54:12 -0500

    completed collections

--- collections_exercice.py
+++ collections_exercice.py
@@ -1,7 +1,30 @@
 #***************************************************collections***************************************
 
 #------------------------------------ counter -------------------------------------------
-from collections import Counter
+""" from collections import Counter
 
 x = 'Hello this is a sentence, lets see how many a it as'
-print(Counter(x))
+print(Counter(x)) #Counter(x) is a dictionary 
+print(Counter(x).items())
+print(Counter(x).values())
+ """
+#----------------------------- namedtuple -------------------------------------------
+
+""" from collections import namedtuple
+
+recta = namedtuple('recta', 'slope, intersection')
+print(recta(5, 3))
+ 
+punto = namedtuple('punto', 'x, y')
+print(punto(3, 2))  """
+
+#----------------------------- deque -------------------------------------------
+
+from collections import deque
+
+d = deque()
+d.append('LUIS')
+d.append('MIGUEL')
+d.append('JIMENEZ')
+d.appendleft('castro')
+print(d)
