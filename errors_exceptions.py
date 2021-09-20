dd44d447e1d7ed104209b0d43b449e7ad0a9847f
Luis Miguel Jimenez <yomiguel112@gmail.com>
2021-09-20 14:06:05 -0500

    completed

--- errors_exceptions.py
+++ errors_exceptions.py
@@ -1,2 +1,13 @@
 #----------------------------Errors and Exceptions-----------------------------------
 
+""" try:
+    x = 5/0
+except:
+    print('division by zero') """
+
+""" x = -5
+assert (x >= 0), 'x is not positive' """
+
+x = -5
+if x < 0:
+    raise Exception('x should be positive')
